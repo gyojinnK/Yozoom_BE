b3b0d39fa851749911a87fd71df57a185d49d98c
gyojinnK <ksmug1998@gmail.com>
2023-10-28 16:25:03 +0900

    pytrends App url 연결

--- yozoom_BE/urls.py
+++ yozoom_BE/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
-from . import craw
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/test/', views.test_view),
-    path('api/craw/', craw.test_craw)
+    path('pytrendapp/', include('pytrendapp.urls'))
 ]
